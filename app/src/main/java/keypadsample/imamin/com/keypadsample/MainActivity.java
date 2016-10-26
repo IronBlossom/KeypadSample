f473444c15d42f12b45a7dbc98f111f8a4762174
Ishtiaq Mahmood Amin <imamin@ael-bd.com>
2016-10-26 14:28:51 +0600

    All pushed

---
+++ app/src/main/java/keypadsample/imamin/com/keypadsample/MainActivity.java
@@ -0,0 +1,22 @@
+package keypadsample.imamin.com.keypadsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onKeyPressed(View v) {
+
+    }
+
+    public void onControlKeyPressed(View v) {
+
+    }
+}
